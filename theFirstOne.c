97e86e8c0f53ff3971ca4ecaa83b34901dc8b915
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2026-01-12 18:20:11 +0000

    reading lines.

--- theFirstOne.c
+++ theFirstOne.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 8
+#define BUFFER_SIZE 100
 
-int main()
+char buffer[BUFFER_SIZE];
+
+void readN(FILE *fptr)
 {
-    FILE *fptr = {fopen("messages.txt", "r")};
-
-    char buffer[BUFFER_SIZE];
-
+    //fread reads n bytes.
     while (fread(buffer, sizeof(char), BUFFER_SIZE, fptr) != 0)
     {
         printf("%s\n", buffer);
     }
+}
+
+void readLine(FILE *fptr)
+{
+    //fgets gets a string of n characters, or gets a string until it finds an end of line.
+    while(fgets(buffer, BUFFER_SIZE, fptr))
+    {
+        printf("%s", buffer);
+    }
+
+}
+
+int main()
+{
+    FILE *fptr = {fopen("messages.txt", "r")};
+    //readN(fptr);
+    readLine(fptr);
 
     return 0;
 }
