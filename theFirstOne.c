5127c47f51036aad03b9ab335a3eb94131631011
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2026-01-12 19:03:59 +0000

    going deeper now!

--- theFirstOne.c
+++ theFirstOne.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 100
 
@@ -14,7 +15,7 @@ void readN(FILE *fptr)
     }
 }
 
-void readLine(FILE *fptr)
+char *readLine(FILE *fptr)
 {
     //fgets gets a string of n characters, or gets a string until it finds an end of line.
     while(fgets(buffer, BUFFER_SIZE, fptr))
@@ -24,11 +25,58 @@ void readLine(FILE *fptr)
 
 }
 
+int getFileSize(FILE *fptr)
+{
+    fseek(fptr, 0, SEEK_END);
+    int s = {ftell(fptr)};
+    rewind(fptr);
+    
+    return s;
+}
+
+int getLines(FILE *fptr)
+{
+    int lc = {0};
+
+    char c;
+    while (c != EOF)
+    {
+        c = fgetc(fptr);
+        if (c == '\n') ++lc;
+    }
+
+    return lc;
+}
+
+int getLineCharCount(FILE *fptr)
+{
+    int cc = {0};
+    
+    char c;
+    while (c != '\n')
+    {
+        ++cc;
+    }
+
+    return cc;
+}
+
+int *getLineCharCount(FILE *fptr)
+{
+    int l = {getLines(fptr)};
+
+    for (int i = 0; i < l; ++i)
+    {
+        
+    }
+}
+
 int main()
 {
     FILE *fptr = {fopen("messages.txt", "r")};
     //readN(fptr);
-    readLine(fptr);
+    //readLine(fptr);
 
+    printf("%d\n%d\n", getFileSize(fptr), getLines(fptr));
     return 0;
 }
